1d9f513bc0b09d91e3d1ea684458e16b9931498f
lu <1358815968@qq.com>
2018-08-05 11:19:34 +0800

    B的第一次

--- day68_solrj/src/main/java/itheima/Test.java
+++ day68_solrj/src/main/java/itheima/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("这是A程序员修改");
         System.out.println("这是B程序员修改");
+        System.out.println("B程序员没有更新,直接写代码了,一定会发生冲突...");
     }
 }
