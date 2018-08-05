a62cd63aa21a44477766d6c286f21da06135f611
lu <1358815968@qq.com>
2018-08-05 11:17:01 +0800

    搞事情

--- day68_solrj/src/main/java/itheima/Test.java
+++ day68_solrj/src/main/java/itheima/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("这是A程序员修改");
         System.out.println("这是B程序员修改");
+        System.out.println("A搞事情了");
     }
 }
